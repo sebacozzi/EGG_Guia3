ce749129c99ad13fa8e9da3e73b4fe9daa59b3a5
sebacozzi <127224339+sebacozzi@users.noreply.github.com>
2023-03-13 18:59:35 -0300

    Carga de Ejercicio 6 - Ejercicio Finalizado con comentarios/1

--- Ejercicios/Ejercicio_7/src/ejercicio_7/Ejercicio_7.java
+++ Ejercicios/Ejercicio_7/src/ejercicio_7/Ejercicio_7.java
@@ -1,20 +1,30 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package ejercicio_7;
 
+import java.util.Scanner;
+
 /**
- *
- * @author Usuario
+ *Realizar un programa que simule el funcionamiento de un dispositivo RS232, 
+ * este tipo de dispositivo lee cadenas enviadas por el usuario. Las cadenas 
+ * deben llegar con un formato fijo: tienen que ser de un máximo de 5 caracteres
+ * de largo, el primer carácter tiene que ser X y el último tiene que ser una O.
+ *Las secuencias leídas que respeten el formato se consideran correctas, la 
+ * secuencia especial “&&&&&” marca el final de los envíos (llamémosla FDE), 
+ * y toda secuencia distinta de FDE, que no respete el formato se considera 
+ * incorrecta.
+ *Al finalizar el proceso, se imprime un informe indicando la cantidad de 
+ * lecturas correctas e incorrectas recibidas. Para resolver el ejercicio deberá 
+ * investigar cómo se utilizan las siguientes funciones de Java 
+ * Substring(), Length(), equals(). 
  */
 public class Ejercicio_7 {
-
-    /**
-     * @param args the command line arguments
-     */
+ 
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner leer = new Scanner(System.in);
+        int cCorrectas,cIncorrectas;
+        
+        do
+            
     }
     
 }
